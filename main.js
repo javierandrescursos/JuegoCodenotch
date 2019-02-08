52d6cedfc442db42e866dd83e516350099e6bf31
javierandrescursos <47427910+javierandrescursos@users.noreply.github.com>
2019-02-08 18:40:20 +0100

    switch case terminado terminado

--- main.js
+++ main.js
@@ -1,219 +1,226 @@
+$(document).ready(function () {
+    var arrayGritito = ["Pio Pio", "Cuak", "qui quiri qui", null];
+    var arrayNombreGallo = ["Claudio", "Kellogs", "ManelNavarro", "Rafaelito"];
+    var arrayFuerzaGallo = [5, 8, 4, 7];
+    var arrayPlayerGallo = ["Rafa", "Jacobo", "JaviProfesor", "Charly"];
+    var arrayVida = [20, 30, 10, 10];
 
-var arrayGritito = ["Pio Pio", "Cuak", "qui quiri qui", null];
-var arrayNombreGallo = ["Claudio", "Kellogs", "ManelNavarro", "Rafaelito"];
-var arrayFuerzaGallo = [5, 8, 4, 7];
-var arrayPlayerGallo = ["Rafa", "Jacobo", "JaviProfesor", "Charly"];
-var arrayVida = [20, 30, 10, 10];
-
-// Esto te lo devulve el Switch Case Relleno
-let tipoAtaque_1 = "";
-let tipoAtaque_2 = "";
-
-
-//Te van a dar un número dependiendo del gallo que hayan elegido
-let gallo_1;
-let gallo_2;
+    // Esto te lo devulve el Switch Case Relleno
+    let tipoAtaque_1 = "";
+    let tipoAtaque_2 = "";
 
 
-class GalloClass {
-    constructor(player, gritito, nombre, fuerza, vida) {
-        this.nombre = nombre;
-        this.gritito = gritito;
-        this.fuerza = Math.floor(Math.random() * fuerza);
-        this.vida = vida;
-        this.player = player;
+    //Te van a dar un número dependiendo del gallo que hayan elegido
+    let gallo_1;
+    let gallo_2;
+
+
+    class GalloClass {
+        constructor(player, gritito, nombre, fuerza, vida) {
+            this.nombre = nombre;
+            this.gritito = gritito;
+            this.fuerza = Math.floor(Math.random() * fuerza);
+            this.vida = vida;
+            this.player = player;
+        }
+
+        darPicotazo() {
+            console.log(" hace un taladracabezas");
+        }
+        darAranazo() {
+            console.log("araña ojos");
+        }
+        darPatadon() {
+            console.log("da una patada segadora barrepatas");
+        }
+
+        get getFuerza() { return this.fuerza; }
+        set setFuerza(valor) { this.fuerza = valor }
+
+        get getVida() { return this.vida; }
+        set setVida(valor) { this.vida = valor }
     }
 
-    darPicotazo() {
-        console.log(" hace un taladracabezas");
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // AQUÍ EMPIEZA LA PARTE DE BÁRBARA: CONTROLES DEL JUEGO
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    class ControlJuego {
+
+        ejecutaAccion(numeroTecla) {
+
+            switch (numeroTecla) {
+                case 65:
+                    tipoAtaque_1 = "Aranazo";
+                    console.log("Estoy dando un aranazo");
+                    break;
+
+                case 83:
+                    tipoAtaque_1 = "Patadon";
+                    console.log("Estoy dando un Patadon");
+                    break;
+
+                case 68:
+                    tipoAtaque_1 = "Picotazo";
+                    console.log("Estoy dando un picotazo");
+                    break;
+
+                case 74:
+                tipoAtaque_2 = "Aranazo";
+                    console.log("Estoy dando un aranazo");
+                    break;
+                case 75:
+                tipoAtaque_2 = "Patadon";
+                    console.log("Estoy dando un Patadon");
+                    break;
+                case 76:
+                tipoAtaque_2 = "Picotazo"
+                    console.log("Estoy dando un picotazo");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
     }
-    darAranazo() {
-        console.log("araña ojos");
+
+    function eligeControl() {
+        let controles = new ControlJuego();
+        $(document).keydown(function (event) {
+            controles.ejecutaAccion(event.which);
+        });
     }
-    darPatadon() {
-        console.log("da una patada segadora barrepatas");
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // AQUÍ TERMINA LA PARTE DE BÁRBARA: CONTROLES DEL JUEGO
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // AQUÍ EMPIEZA LA PARTE DE JACOBO: LÓGICA DEL SELECTOR
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    var imagenesGallos = ["gallo1.png", "gallo2.png", "gallo3.png", "gallo4.png"];
+    var descripcionGallos = ["Este es nuestro Gallo #1. Su nombre es Kellogs. Tiene una fuerza máxima de 7 y una salud máxima de 20.",
+        "Este es nuestro Gallo #2. Su nombre es Manel Navarro. Tiene una fuerza máxima de 5 y una salud máxima de 25.",
+        "Este es nuestro Gallo #3. Su nombre es Claudio. Tiene una fuerza máxima de 8 y una salud máxima de 18.",
+        "Este es nuestro Gallo #4. Su nombre es Rafaelito. Tiene una fuerza máxima de 10 y una salud máxima de 13."
+    ]
+    var fraccionGallos = ["1 / 4", "2 / 4", "3 / 4", "4 / 4",]
+
+    var x = 0;
+
+    function siguiente() {
+        var sliderImagen = document.getElementById("imagenesGallosId");
+        var sliderDescripcion = document.getElementById("descripcionId");
+        var sliderFraccion = document.getElementById("fraccionId");
+
+        x++;
+
+        if (x >= imagenesGallos.length) {
+            x = 0;
+        }
+
+        sliderImagen.src = imagenesGallos[x];
+        sliderDescripcion.innerHTML = descripcionGallos[x];
+        sliderFraccion.innerHTML = fraccionGallos[x];
     }
 
-    get getFuerza() { return this.fuerza; }
-    set setFuerza(valor) { this.fuerza = valor }
+    function anterior() {
+        var sliderImagen = document.getElementById("imagenesGallosId");
+        var sliderDescripcion = document.getElementById("descripcionId");
+        var sliderFraccion = document.getElementById("fraccionId");
 
-    get getVida() { return this.vida; }
-    set setVida(valor) { this.vida = valor }
-}
+        x--;
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// AQUÍ EMPIEZA LA PARTE DE BÁRBARA: CONTROLES DEL JUEGO
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        if (x < 0) {
+            x = imagenesGallos.length - 1;
+        }
 
-class ControlJuego {
+        sliderImagen.src = imagenesGallos[x];
+        sliderDescripcion.innerHTML = descripcionGallos[x];
+        sliderFraccion.innerHTML = fraccionGallos[x];
+    }
 
-    ejecutaAccion(numeroTecla) {
+    function siguiente2() {
+        var sliderImagen = document.getElementById("imagenesGallosId2");
+        var sliderDescripcion = document.getElementById("descripcionId2");
+        var sliderFraccion = document.getElementById("fraccionId2");
 
-        switch (numeroTecla) {
-            case 65:
-                console.log("aranazo");
-                break;
-            case 83:
-                console.log("Patadon");
-                break;
+        x++;
 
-            case 68:
-                console.log("cabezazo");
-                break;
+        if (x >= imagenesGallos.length) {
+            x = 0;
+        }
+        sliderImagen.src = imagenesGallos[x];
+        sliderDescripcion.innerHTML = descripcionGallos[x];
+        sliderFraccion.innerHTML = fraccionGallos[x];
 
-            case 74:
-                console.log("aranazo");
-                break;
-            case 75:
-                console.log("Patadon");
-                break;
-            case 76:
-                console.log("cabezazo");
-                break;
 
-            default:
-                break;
+    }
+
+    function anterior2() {
+        var sliderImagen = document.getElementById("imagenesGallosId2");
+        var sliderDescripcion = document.getElementById("descripcionId2");
+        var sliderFraccion = document.getElementById("fraccionId2");
+
+        x--;
+
+        if (x < 0) {
+            x = imagenesGallos.length - 1;
+        }
+        sliderImagen.src = imagenesGallos[x];
+        sliderDescripcion.innerHTML = descripcionGallos[x];
+        sliderFraccion.innerHTML = fraccionGallos[x];
+    }
+
+    function crearGallo(i) {
+        var gallo = new GalloClass(arrayPlayerGallo[i], arrayGritito[i], arrayNombreGallo[i], arrayFuerzaGallo[i], arrayVida[i]);
+        return gallo;
+    }
+
+    function comparacionAtaque(tipoAtaque_1, tipoAtaque_2) {
+        if (tipoAtaque_1 === "Patadon") {
+            if (tipoAtaque_2 === "Picotazo") {
+                return;
+            } else if (tipoAtaque_2 === "Aranazo") {
+                return 2;
+            }
+        } else if (tipoAtaque_1 === "Picotazo") {
+            if (tipoAtaque_2 === "Patadon") {
+                return 2;
+            } else if (tipoAtaque_2 === "Aranazo") {
+                return 1;
+            }
+        } else if (tipoAtaque_1 === "Aranazo") {
+            if (tipoAtaque_2 === "Patadon") {
+                return 2;
+            } else if (tipoAtaque_2 === "Picotazo") {
+                return 1;
+            }
+        } else {
+            return 0;
         }
     }
 
-}
-
-function eligeControl() {
-    let controles = new ControlJuego();
-    $(document).keydown(function (event) {
-        controles.ejecutaAccion(event.which);
-    });
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// AQUÍ TERMINA LA PARTE DE BÁRBARA: CONTROLES DEL JUEGO
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// AQUÍ EMPIEZA LA PARTE DE JACOBO: LÓGICA DEL SELECTOR
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-var imagenesGallos = ["images/gallo1.png", "images/gallo2.png", "images/gallo3.png", "images/gallo4.png"];
-var descripcionGallos = ["Este es nuestro Gallo #1. Su nombre es Kellogs. Tiene una fuerza máxima de 7 y una salud máxima de 20.",
-    "Este es nuestro Gallo #2. Su nombre es Manel Navarro. Tiene una fuerza máxima de 5 y una salud máxima de 25.",
-    "Este es nuestro Gallo #3. Su nombre es Claudio. Tiene una fuerza máxima de 8 y una salud máxima de 18.",
-    "Este es nuestro Gallo #4. Su nombre es Rafaelito. Tiene una fuerza máxima de 10 y una salud máxima de 13."
-]
-var fraccionGallos = ["1 / 4", "2 / 4", "3 / 4", "4 / 4",]
-
-var x = 0;
-
-function siguiente() {
-    var sliderImagen = document.getElementById("imagenesGallosId");
-    var sliderDescripcion = document.getElementById("descripcionId");
-    var sliderFraccion = document.getElementById("fraccionId");
-
-    x++;
-
-    if (x >= imagenesGallos.length) {
-        x = 0;
-    }
-
-    sliderImagen.src = imagenesGallos[x];
-    sliderDescripcion.innerHTML = descripcionGallos[x];
-    sliderFraccion.innerHTML = fraccionGallos[x];
-}
-
-function anterior() {
-    var sliderImagen = document.getElementById("imagenesGallosId");
-    var sliderDescripcion = document.getElementById("descripcionId");
-    var sliderFraccion = document.getElementById("fraccionId");
-
-    x--;
-
-    if (x < 0) {
-        x = imagenesGallos.length - 1;
-    }
-
-    sliderImagen.src = imagenesGallos[x];
-    sliderDescripcion.innerHTML = descripcionGallos[x];
-    sliderFraccion.innerHTML = fraccionGallos[x];
-}
-
-function siguiente2() {
-    var sliderImagen = document.getElementById("imagenesGallosId2");
-    var sliderDescripcion = document.getElementById("descripcionId2");
-    var sliderFraccion = document.getElementById("fraccionId2");
-
-    x++;
-
-    if (x >= imagenesGallos.length) {
-        x = 0;
-    }
-    sliderImagen.src = imagenesGallos[x];
-    sliderDescripcion.innerHTML = descripcionGallos[x];
-    sliderFraccion.innerHTML = fraccionGallos[x];
-
-
-}
-
-function anterior2() {
-    var sliderImagen = document.getElementById("imagenesGallosId2");
-    var sliderDescripcion = document.getElementById("descripcionId2");
-    var sliderFraccion = document.getElementById("fraccionId2");
-
-    x--;
-
-    if (x < 0) {
-        x = imagenesGallos.length - 1;
-    }
-    sliderImagen.src = imagenesGallos[x];
-    sliderDescripcion.innerHTML = descripcionGallos[x];
-    sliderFraccion.innerHTML = fraccionGallos[x];
-}
-
-function crearGallo(i) {
-    var gallo = new GalloClass(arrayPlayerGallo[i], arrayGritito[i], arrayNombreGallo[i], arrayFuerzaGallo[i], arrayVida[i]);
-    return gallo;
-}
-
-function comparacionAtaque(tipoAtaque_1, tipoAtaque_2) {
-    if (tipoAtaque_1 === "Patadon") {
-        if (tipoAtaque_2 === "Picotazo") {
-            return;
-        } else if (tipoAtaque_2 === "Aranazo") {
-            return 2;
+    function quitarVida(tipoAtaque_1, tipoAtaque_2) {
+        if (comparacionAtaque(tipoAtaque_1, tipoAtaque_2) == 1) {
+            gallo_2.setVida = gallo_2.getVida - gallo_1.getFuerza;
+            console.log(gallo_1.nombre + " le ha quitado " + gallo_2.getFuerza + " de fuerza a " + gallo_2.nombre);
+        } else if (comparacionAtaque(tipoAtaque_1, tipoAtaque_2) == 2) {
+            gallo_1.setVida = getVida - gallo_1.getFuerza;
+            console.log(gallo_2.nombre + " le ha quitado " + gallo_1.getFuerza + " de fuerza a " + gallo_1.nombre);
+        } else {
+            console.log('Empate');
         }
-    } else if (tipoAtaque_1 === "Picotazo") {
-        if (tipoAtaque_2 === "Patadon") {
-            return 2;
-        } else if (tipoAtaque_2 === "Aranazo") {
-            return 1;
-        }
-    } else if (tipoAtaque_1 === "Aranazo") {
-        if (tipoAtaque_2 === "Patadon") {
-            return 2;
-        } else if (tipoAtaque_2 === "Picotazo") {
-            return 1;
-        }
-    } else {
-        return 0;
     }
-}
 
-
-
-function quitarVida(tipoAtaque_1, tipoAtaque_2) {
-    if (comparacionAtaque(tipoAtaque_1, tipoAtaque_2) == 1) {
-        gallo_2.setVida = gallo_2.getVida - gallo_1.getFuerza;
-        console.log(gallo_1.nombre + " le ha quitado " + gallo_2.getFuerza + " de fuerza a " + gallo_2.nombre);
-    } else if (comparacionAtaque(tipoAtaque_1, tipoAtaque_2) == 2) {
-        gallo_1.setVida = getVida - gallo_1.getFuerza;
-        console.log(gallo_2.nombre + " le ha quitado " + gallo_1.getFuerza + " de fuerza a " + gallo_1.nombre);
-    } else {
-        console.log('Empate');
-    }
-}
-
-
+});
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // AQUÍ TERMINA LA PARTE DE JACOBO: LÓGICA DEL SELECTOR
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -360,7 +367,43 @@ function quitarVida(tipoAtaque_1, tipoAtaque_2) {
 
 
 // }
-function crearGallosElegidos(numeroGallo1,numeroGallo2){
+function crearGallosElegidos(numeroGallo1, numeroGallo2) {
     gallo_1 = crearGallo(numeroGallo1);
     gallo_2 = crearGallo(numeroGallo2);
 }
+
+var vida = 100;
+var idIterval = setInterval(function () {
+    // disminuye en 5 el progeso
+    vida -= 20;
+    $('#bar').css('width', vida + '%');
+    var img = document.getElementById("imagengallo")
+    if (vida > 25) {
+        img.src = "images/gallo4.png"
+    }
+    else {
+        img.src = "images/gallo4herido.png";
+    };
+    //Si llegó a 0 elimino el intervalo
+    if (vida == 0) {
+        alert("KO");
+    }
+}, 800)
+
+var vida2 = 100;
+var idIterval = setInterval(function () {
+    // disminuye en 5 el progeso
+    vida2 -= 10;
+    $('#bar2').css('width', vida2 + '%');
+    var img = document.getElementById("imagengallo2")
+    if (vida2 > 25) {
+        img.src = "images/gallo2.png"
+    }
+    else {
+        img.src = "images/gallo2herido.png";
+    };
+    //Si llegó a 0 elimino el intervalo
+    if (vida2 == 0) {
+        alert("KO");
+    }
+}, 800)
